49b453fe56a5a9a88f597abe2d35c55648f93e7a
Master1624 <jdpa83@gmail.com>
2025-08-09 18:57:21 -0500

    feat: Implement translations

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,16 +1,16 @@
 import { Button } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export function Welcome() {
+  const { t } = useTranslation();
   return (
     <div
       transition-style="in:circle:bottom-right"
       className="flex flex-col items-center justify-center h-screen space-y-1.5"
     >
       <div>
-        <h1>¡ME PRESENTO! MI NOMBRE ES JUAN DAVID PALADINES AMAYA</h1>
-        <p>
-          Soy un desarrollador frontend con experiencia en React y TypeScript.
-        </p>
+        <h1>{t("welcome.title")}</h1>
+        <p>{t("welcome.description")}</p>
       </div>
       <div>
         <Button>Botón 1</Button>
