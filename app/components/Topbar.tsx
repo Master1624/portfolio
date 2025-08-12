2a0ca53a41f0279122ec7a1355043aed565a2bf3
Master1624 <jdpa83@gmail.com>
2025-08-12 18:11:50 -0500

    feat: Add Home button in Topbar

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -1,24 +1,30 @@
 import { Button, Card, CardContent } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 
 export function Topbar() {
   const navigate = useNavigate();
   const { i18n } = useTranslation();
+  const { pathname } = useLocation();
 
   const otherLanguage = Object.keys(i18n.options.resources || {}).find(
     (lng) => lng !== i18n.language
   );
 
   const handleLanguageChange = async () => {
-    await navigate("/", { replace: true });
+    await navigate(pathname, { replace: true });
     i18n.changeLanguage(otherLanguage);
   };
 
   return (
     <Card className="fixed top-0 w-full z-10">
-      <CardContent className="flex w-full justify-end items-center">
-        <Button onClick={handleLanguageChange}>{otherLanguage}</Button>
+      <CardContent className="flex w-full justify-between items-center">
+        <Button>
+          <Link to="/">Home</Link>
+        </Button>
+        <>
+          <Button onClick={handleLanguageChange}>{otherLanguage}</Button>
+        </>
       </CardContent>
     </Card>
   );
