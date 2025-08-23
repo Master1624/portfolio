a0299b20a629f66ad17d16c28b880f19a305eff0
Master1624 <jdpa83@gmail.com>
2025-08-23 14:09:29 -0500

    refactor: Implement responsive view in Experiences view

--- app/modules/experiences/Experiences.tsx
+++ app/modules/experiences/Experiences.tsx
@@ -5,11 +5,13 @@ import { useTranslation } from "react-i18next";
 export function Experiences() {
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col items-center justify-center justify-items-center h-screen space-y-2">
-      <div className="fixed top-1/7">
-        <h1 className="text-5xl font-bold text-black dark:text-white">{t('common.experiences')}</h1>
+    <div className="flex flex-col items-center justify-center justify-items-center space-y-2">
+      <div className="sm:fixed sm:top-1/7 sm:mt-0 max-sm:hidden">
+        <h1 className="text-5xl font-bold text-black dark:text-white">
+          {t("common.experiences")}
+        </h1>
       </div>
-      <div className="flex flex-row flex-wrap content-center justify-center gap-7 overflow-y-auto h-1/2 w-screen">
+      <div className="flex flex-row flex-wrap content-center justify-center gap-7 overflow-y-auto max-sm:mt-20">
         {experiences.map((experience) => (
           <Experience key={experience.company} experience={experience} />
         ))}
